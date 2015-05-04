e5570ec96fcc2442ffabc49c05616954884f91d6
Wedika Jadhav <vedikajadhav48@gmail.com>
2015-05-03 21:02:00 -0700

    Trial Commit

--- src/com/mitchell/examples/services/MitchellClaimMain.java
+++ src/com/mitchell/examples/services/MitchellClaimMain.java
@@ -19,7 +19,7 @@ import com.mitchell.examples.claim.ObjectFactory;
 public class MitchellClaimMain {
 	public static void main(String[] args){
 		System.out.println("1. Create Claim \n2.Read Claim \n3.FindListOfClaimByDateRange \n4.Update Claim \n5.Read a specific vehicle \n6. Delete a claim");
-		
+		System.out.println("Enter your choice");
 		try{
 			MitchellClaimBackingStore mitchellClaimStore = new MitchellClaimBackingStore();
 			File file = new File("create-claim.xml");
